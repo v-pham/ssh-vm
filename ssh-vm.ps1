539dd3e75c4049363a00592e7f5ee287a85965f4
v-pham <v-pham@users.noreply.github.com>
2021-12-13 03:30:30 -0500

    Update ssh-vm.ps1
    Added param specify a different login name, a different Hyper-V host (the autocompletion should dynamically change) as well as some meaningful verbose and warning messages. Added "Force" switch to attempt to start VMs that may be stopped.

--- ssh-vm.ps1
+++ ssh-vm.ps1
@@ -2,32 +2,74 @@
 # Highly recommend adding user accounts to Hyper-V Administrators group (otherwise, this function would require Administrator rights to work).
 
 function Invoke-SshToVM {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="Default")]
     [Alias("ssh-vm")]
-    Param ()
+    Param (
+        [Parameter(Mandatory=$false)][string]$VMHost=$Env:COMPUTERNAME,
+        [Parameter(ParameterSetName="Default",Mandatory=$false)][switch]$ClipboardIPAddressOnly,
+        [Parameter(ParameterSetName="AltLogin",Mandatory=$false)][string]$LoginName='',
+        [Parameter(Mandatory=$false)][switch]$Force
+    )
     DynamicParam {
-        $ParameterName = 'VM'
+        $ParameterName = 'VMName'
         $RuntimeParameterDictionary = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
         $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
         $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
         $ParameterAttribute.Mandatory = $true
         $ParameterAttribute.Position = 0
         $AttributeCollection.Add($ParameterAttribute)
-        $arrSet = Get-VM | Select-Object -ExpandProperty Name
-        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
+        if(!$PSBoundParameters.ContainsKey('VMHost')){
+            $VMHost=$Env:COMPUTERNAME
+        }
+        [string[]]$ParamSet = Get-VM -ComputerName $VMHost | Where-Object { $_.State -eq 'Running' } | Select-Object -ExpandProperty Name
+        if($ParamSet.Count -eq 0 -or $Force.IsPresent){
+            $WarningMessage = 'Autocompletion will include all VMs but will require -Force to attempt to start the VM before invoking SSH.'
+            if($ParamSet.Count -eq 0){
+                $NoVMsRunning = $true
+                $WarningMessage = "No running VMs were found. " + $WarningMessage
+            }
+            Write-Warning -Message $WarningMessage
+            [string[]]$ParamSet = Get-VM | Select-Object -ExpandProperty Name
+        }
+        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($ParamSet)
         $AttributeCollection.Add($ValidateSetAttribute)
+
         $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
         $RuntimeParameterDictionary.Add($ParameterName, $RuntimeParameter)
         return $RuntimeParameterDictionary
     }
 
     begin {
-        $VM = $PsBoundParameters[$ParameterName]
+        if(!$PSBoundParameters.ContainsKey('VMHost')){
+            $VMHost=$Env:COMPUTERNAME
+        }
+        $VMName = $PsBoundParameters[$ParameterName]
+        if($LoginName.Length -gt 0){
+            $LoginName = $LoginName + "@"
+        }else{
+            $LoginName = ""
+        }
     }
 
     process{
-        [array]$IpAddress = Get-VMNetworkAdapter -VMName $VM | Select-Object -ExpandProperty IPAddresses
-        # Added option to ignore HostKeyChecking since the IPs are constantly changing (which is why this function was written in the first place)
-        Invoke-Expression -Command "ssh -o StrictHostKeyChecking=no $($IpAddress[0])"
+        if($NoVMsRunning -and !$Force.IsPresent){
+            Write-Error "The requested VM was found but not running." -RecommendedAction "Either start the VM or use the -Force parameter to attempt to start the VM prior to invoking SSH." -ErrorAction Stop
+        }
+        if($Force.IsPresent){
+            if($(Get-VM -ComputerName $VMHost -VMName $VMName).State -ne 'Running'){
+                Start-VM -ComputerName $VMHost -VMName $VMName
+                Start-Sleep -Seconds 5
+            }
+        }
+        [array]$IpAddresses = Get-VMNetworkAdapter -ComputerName $VMHost -VMName $VMName | Select-Object -ExpandProperty IPAddresses
+        if($IpAddresses.Count -eq 0){
+            Write-Error 'No IP address could be found on network adapter.' -RecommendedAction 'Ensure Guest Services are enabled and are running on the virtual machine.' -ErrorAction Stop
+        }
+        if($ClipboardIPAddressOnly.IsPresent){
+            Set-Clipboard -Value $IpAddresses[0]
+        }else {
+            Write-Verbose "Invoking command: ssh -o StrictHostKeyChecking=no $LoginName$($IpAddresses[0])"
+            Invoke-Expression -Command "ssh -o StrictHostKeyChecking=no $LoginName$($IpAddresses[0])"
+        }
     }
 }
